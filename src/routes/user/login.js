d73b75fe9dc87df79a4b5c748e2d2b96090d9449
Timmy <timmy.detroch@outlook.com>
2021-07-16 21:34:03 +0200

    jwt update

--- src/routes/user/login.js
+++ src/routes/user/login.js
@@ -14,7 +14,6 @@ async function login(req, res) {
       },
     });
     console.log(result);
-
     //si il existe, je compare le mot de passe et le hash de l'user dans la db
     if (result != null) {
       console.log("l'utilsiateur existe");
@@ -23,18 +22,14 @@ async function login(req, res) {
       if (exist == true) {
         console.log("il est le bon mot de passe et est testé");
         //on attribue à l'utilisateur un token qui reprend son id et son nom si c'est le bon mdp
-        const utilisateur = {
+        const user = {
           id: result.id,
           username: result.username,
+          admin: result.admin,
         };
-        const token = jwt.sign(
-          {
-            id: result.id,
-            username: result.username,
-          },
-          process.env.JWT_SECRET,
-          { expiresIn: "3h" }
-        );
+        const token = jwt.sign(user, process.env.JWT_SECRET, {
+          expiresIn: "3h",
+        });
         res.status(200).send(token);
       }
       //Si le mot de passe n'est pas bon, on prévient l'utilisateur que son mot de passe ou son login entré est incorrect
--- src/routes/wine/deleteAdmin.js
+++ src/routes/wine/deleteAdmin.js
@@ -1,18 +1,22 @@
 const { prisma } = require("../../prisma");
 
 async function deleteWineAdmin(req, res) {
-  try {
-    await prisma.wine.delete({
-      where: {
-        id: req.body.id,
-      },
-    });
+  if (req.user.admin == true) {
+    try {
+      await prisma.wine.delete({
+        where: {
+          id: req.body.id,
+        },
+      });
 
-    res.status(200).send("deleted wine");
-  } catch (error) {
-    console.log(error);
+      res.status(200).send("deleted wine");
+    } catch (error) {
+      console.log(error);
 
-    res.status(400).send("erreur");
+      res.status(400).send("erreur");
+    }
+  } else {
+    res.status(403).send("Accès refusé, vous n'êtes pas administrateur");
   }
 }
 
