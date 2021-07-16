eca7c19c7ebcab51b587fc9253890f3413cc583a
Timmy <timmy.detroch@outlook.com>
2021-07-16 19:07:54 +0200

    middlewares update

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -3,7 +3,7 @@ const jwtMiddleware = expressjwt({
   secret: process.env.JWT_SECRET,
   algorithms: ["HS256"],
 }).unless({
-  path: ["/loginUsers", "/getUsers"],
+  path: ["/users/login", "/users"],
 });
 
 module.exports = {
