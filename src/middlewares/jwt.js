2253f33d01fcdc0b6e6fbb25041ade27a842c520
Timmy <timmy.detroch@outlook.com>
2021-07-26 17:12:13 +0200

    test

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -2,9 +2,11 @@ const expressjwt = require("express-jwt")
 const jwtMiddleware = expressjwt({
   secret: process.env.JWT_SECRET,
   algorithms: ["HS256"]
-}).unless({
-  path: ["/users/login", "/users/create", "/home", "/cave/list"]
+}) /*
+.unless({
+  path: ["/users/login", "/users/create", "/home"]
 })
+*/
 
 module.exports = {
   jwtMiddleware
